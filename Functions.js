f85b1ab4dc8b17d905f20bd7d03f0f93a4a11918
Sam Montanaro <sam.montanaro12@gmail.com>
2019-07-23 01:13:02 -0400

    Version 0.20
    Added 4 new achievements with their own images and upgrades.
    All numbers over 1000 are converted to short form.
    Proper reset button created for testing purposes.
    Improved general layout.

--- Functions.js
+++ Functions.js
@@ -1,5 +1,7 @@
+'use strict';
 let bits = 0;
-let clickBitsAmount = 1;
+let bitsPerSec = 0;
+let bitsPerClick = 1;
 
 let keyloggerAmount = 0;
 let keyloggerPrice = 10;
@@ -10,28 +12,61 @@ let botnetPrice = 1000;
 let superComputerAmount = 0;
 let superComputerPrice = 10000;
 
-function pageInit() {
+let clickerAchievements = [500, 2500, 5000, 9001];
+let clickerUpgradesUnlocked = [false, false, false, false];
+let clickerUpgradesBought = [false, false, false, false];
+let clickerUpgrades = [];
+let buttonClicked = 0;
+
+let largeNums = [1000, 1000000, 1000000000, 1000000000000, 1000000000000000, 1000000000000000000, 1000000000000000000000, 1000000000000000000000000, 1000000000000000000000000000, 1000000000000000000000000000000, 1000000000000000000000000000000000];
+let largeNumAbbr = ["K", "M", "B", "T", "Qa", "Qi", "Sx", "Sp", "Oc", "No", "Dc"];
+
+window.onload = function() {
 	let closeBtn = document.getElementById("closeBtn");
-	let clickerBtn = document.getElementById("clickerButton");
 	let saveBtn = document.getElementById("saveBtn");
 	let loadBtn = document.getElementById("loadBtn");
-	
+	let resetBtn = document.getElementById("resetBtn");
+
+	let clickerBtn = document.getElementById("clickerButton");
 	let keyloggerBtn = document.getElementById("keyloggerBtn");
 	let phishingSiteBtn = document.getElementById("phishingSiteBtn");
 	let botnetBtn = document.getElementById("botnetBtn");
 	let superComputerBtn = document.getElementById("superComputerBtn");
 
+	let CU1 = document.getElementById("clickerUpgrade1");
+	let CU2 = document.getElementById("clickerUpgrade2");
+	let CU3 = document.getElementById("clickerUpgrade3");
+	let CU4 = document.getElementById("clickerUpgrade4");
+	clickerUpgrades = [CU1, CU2, CU3, CU4];
+
 	closeBtn.addEventListener('click', closeModal);
 	clickerBtn.addEventListener('click', clickerButtonClicked);
 	saveBtn.addEventListener('click', saveGameState);
 	loadBtn.addEventListener('click', loadGameState);
+	resetBtn.addEventListener('click', resetGameState);
 
 	keyloggerBtn.addEventListener('click', function(){buyUpgrade(1)});
 	phishingSiteBtn.addEventListener('click', function(){buyUpgrade(2)});
 	botnetBtn.addEventListener('click', function(){buyUpgrade(3)});
 	superComputerBtn.addEventListener('click', function(){buyUpgrade(4)});
 	
+	CU1.addEventListener('click', function(){buyUpgrade(5)});
+	CU1.addEventListener('mouseenter', function(){document.getElementById("CU1-hoverText").style.display = "block";});
+	CU1.addEventListener('mouseout', function(){document.getElementById("CU1-hoverText").style.display = "none";});
+	CU2.addEventListener('click', function(){buyUpgrade(6)});
+	CU2.addEventListener('mouseenter', function(){document.getElementById("CU2-hoverText").style.display = "block";});
+	CU2.addEventListener('mouseout', function(){document.getElementById("CU2-hoverText").style.display = "none";});
+	CU3.addEventListener('click', function(){buyUpgrade(7)});
+	CU3.addEventListener('mouseenter', function(){document.getElementById("CU3-hoverText").style.display = "block";});
+	CU3.addEventListener('mouseout', function(){document.getElementById("CU3-hoverText").style.display = "none";});
+	CU4.addEventListener('click', function(){buyUpgrade(8)});
+	CU4.addEventListener('mouseenter', function(){document.getElementById("CU4-hoverText").style.display = "block";});
+	CU4.addEventListener('mouseout', function(){document.getElementById("CU4-hoverText").style.display = "none";});
+
 	loadGameState();
+	updatePriceChanges();
+	checkUpgradesAffordability();
+	printBits();
 }
 
 window.onclick = function(event) {
@@ -45,13 +80,40 @@ function closeModal() {
 }
 
 function clickerButtonClicked() {
+	buttonClicked++;
+	if (obj !== null)
+		obj.clickerAchievementTracker();
 	animateButton();
-	updateBits(clickBitsAmount);
+	updateBits(bitsPerClick);
 }
 
 function updateBits(amount) {
 	bits += amount;
+	checkUpgradesAffordability();
 	printBits();
+}
+
+function checkUpgradesAffordability() {
+	var upgradePrices = [
+		keyloggerPrice,
+		phishingSitePrice,
+		botnetPrice,
+		superComputerPrice
+	];
+
+	var upgrades = [
+		keyloggerBtn,
+		phishingSiteBtn,
+		botnetBtn,
+		superComputerBtn
+	];
+
+	for (let [i, price] of upgradePrices.entries()) {
+		if (bits < price)
+			upgrades[i].classList.add("greyedOut");
+		else if (upgrades[i].classList.contains("greyedOut"))
+			upgrades[i].classList.remove("greyedOut");
+	}
 }
 
 function animateButton() {
@@ -63,17 +125,16 @@ function animateButton() {
 }
 
 function saveGameState() {
-	updatePriceChanges();
-
 	localStorage.setItem('bits', bits);
+	localStorage.setItem('buttonClicked', buttonClicked);
+	localStorage.setItem('bitsPerClick', bitsPerClick);
 	localStorage.setItem('keyloggerAmount', keyloggerAmount);
-	localStorage.setItem('keyloggerPrice', keyloggerPrice);
 	localStorage.setItem('phishingSiteAmount', phishingSiteAmount);
-	localStorage.setItem('phishingSitePrice', phishingSitePrice);
 	localStorage.setItem('botnetAmount', botnetAmount);
-	localStorage.setItem('botnetPrice', botnetPrice);
 	localStorage.setItem('superComputerAmount', superComputerAmount);
-	localStorage.setItem('superComputerPrice', superComputerPrice);
+	
+	localStorage.setItem('clickerUpgradesUnlocked', JSON.stringify(clickerUpgradesUnlocked));
+	localStorage.setItem('clickerUpgradesBought', JSON.stringify(clickerUpgradesBought));
 }
 
 function autoSavePopup() {
@@ -87,28 +148,66 @@ function autoSavePopup() {
 function loadGameState() {
 	if (localStorage.getItem('bits') !== null) {
 		bits = parseFloat(localStorage.getItem('bits'));
-		printBits();
+	}
+	if (localStorage.getItem('buttonClicked') !== null) {
+		buttonClicked = parseInt(localStorage.getItem('buttonClicked'));
+	}
+	if (localStorage.getItem('bitsPerClick', bitsPerClick) !== null) {
+		bitsPerClick = parseFloat(localStorage.getItem('bitsPerClick'));
 	}
 	if (localStorage.getItem('keyloggerAmount') !== null) {
 		keyloggerAmount = parseInt(localStorage.getItem('keyloggerAmount'));
-		keyloggerPrice = parseInt(localStorage.getItem('keyloggerPrice'));
 		printKeyloggers();
 	}
 	if (localStorage.getItem('phishingSiteAmount') !== null) {
 		phishingSiteAmount = parseInt(localStorage.getItem('phishingSiteAmount'));
-		phishingSitePrice = parseInt(localStorage.getItem('phishingSitePrice'));
 		printPhishingSites();
 	}
 	if (localStorage.getItem('botnetAmount') !== null) {
 		botnetAmount = parseInt(localStorage.getItem('botnetAmount'));
-		botnetPrice = parseInt(localStorage.getItem('botnetPrice'));
 		printBotnets();
 	}
 	if (localStorage.getItem('superComputerAmount') !== null) {
 		superComputerAmount = parseInt(localStorage.getItem('superComputerAmount'));
-		superComputerPrice = parseInt(localStorage.getItem('superComputerPrice'));
 		printSuperComputers();
 	}
+	if (JSON.parse(localStorage.getItem('clickerUpgradesUnlocked')) !==null) {
+		clickerUpgradesUnlocked = JSON.parse(localStorage.getItem('clickerUpgradesUnlocked'));
+	}
+	if (JSON.parse(localStorage.getItem('clickerUpgradesBought')) !== null) {
+		clickerUpgradesBought = JSON.parse(localStorage.getItem('clickerUpgradesBought'));
+		for (let [i, upgrade] of clickerUpgradesBought.entries()) {
+			if (upgrade === true)
+				clickerUpgrades[i].style.display = "none";
+			else if (clickerUpgradesUnlocked[i] === true)
+				clickerUpgrades[i].style.display = "inline-block";
+		}
+	}
+	updateBitsPerClick();
+	updatePriceChanges();
+	printBits();
+}
+
+function resetGameState() {
+	bits = 0;
+	buttonClicked = 0;
+	bitsPerClick = 1;
+	keyloggerAmount = 0;
+	phishingSiteAmount = 0;
+	botnetAmount = 0;
+	superComputerAmount = 0;
+	updatePriceChanges();
+	
+	for (let [i, v] of clickerUpgradesBought.entries())
+		clickerUpgradesBought[i] = false;
+	for (let [i, v] of clickerUpgradesUnlocked.entries())
+		clickerUpgradesUnlocked[i] = false;
+	for (let [i, v] of clickerUpgrades.entries())
+		clickerUpgrades[i].style.display = "none";
+
+	localStorage.clear();
+	saveGameState();
+	loadGameState();
 }
 
 function buyUpgrade(selector) {
@@ -119,41 +218,102 @@ function buyUpgrade(selector) {
 				keyloggerAmount++;
 				keyloggerPrice = Math.floor(10 * Math.pow(1.1, keyloggerAmount));
 				
+				updateBitsPerClick();
 				printBits();
+				checkUpgradesAffordability();
 				printKeyloggers();
-				break;
 			}
+			break;
 		case 2:
 			if (bits >= phishingSitePrice) {
 				bits -= phishingSitePrice;
 				phishingSiteAmount++;
 				phishingSitePrice = Math.floor(100 * Math.pow(1.15, phishingSiteAmount));
 
+				updateBitsPerClick();
 				printBits();
+				checkUpgradesAffordability();
 				printPhishingSites();
-				break;
 			}
+			break;
 		case 3:
 			if (bits >= botnetPrice) {
 				bits -= botnetPrice;
 				botnetAmount++;
 				botnetPrice = Math.floor(1000 * Math.pow(1.2, botnetAmount));
 
+				updateBitsPerClick();
 				printBits();
+				checkUpgradesAffordability();
 				printBotnets();
-				break;
 			}
+			break;
 		case 4:
 			if (bits >= superComputerPrice) {
 				bits -= superComputerPrice;
 				superComputerAmount++;
 				superComputerPrice = Math.floor(10000 * Math.pow(1.25, superComputerAmount));
 
+				updateBitsPerClick();
 				printBits();
+				checkUpgradesAffordability();
 				printSuperComputers();
-				break;
 			}
+			break;
+		case 5:
+			if (bits >= 750) {
+				bits -= 750;
+				clickerUpgradesBought[0] = true;
+				document.getElementById("clickerUpgrade1").style.display = "none";
+
+				updateBitsPerClick();
+				printBits();
+				checkUpgradesAffordability();
+			}
+			break;
+		case 6:
+			if (bits >= 5000) {
+				bits -= 5000;
+				clickerUpgradesBought[1] = true;
+				document.getElementById("clickerUpgrade2").style.display = "none";
+
+				updateBitsPerClick();
+				printBits();
+				checkUpgradesAffordability();
+			}
+			break;
+		case 7:
+			if (bits >= 10000) {
+				bits -= 10000;
+				clickerUpgradesBought[2] = true;
+				document.getElementById("clickerUpgrade3").style.display = "none";
+
+				updateBitsPerClick();
+				printBits();
+				checkUpgradesAffordability();
+			}
+			break;
+		case 8:
+			if (bits >= 20000) {
+				bits -= 20000;
+				clickerUpgradesBought[3] = true;
+				document.getElementById("clickerUpgrade4").style.display = "none";
+
+				updateBitsPerClick();
+				printBits();
+				checkUpgradesAffordability();
+			}
+			break;
 	}
+}
+
+function updateBitsPerClick() {
+	bitsPerClick = 1;
+	for (let [i, upgrade] of clickerUpgradesBought.entries()) {
+		if (upgrade)
+			bitsPerClick += (bitsPerSec * ((i + 1) / 100));
+	}
+	printBits();
 }
 
 function updatePriceChanges() {
@@ -162,6 +322,7 @@ function updatePriceChanges() {
 	botnetPrice = Math.floor(1000 * Math.pow(1.2, botnetAmount));
 	superComputerPrice = Math.floor(10000 * Math.pow(1.25, superComputerAmount));
 
+	checkUpgradesAffordability();
 	printKeyloggers();
 	printPhishingSites();
 	printBotnets();
@@ -169,29 +330,45 @@ function updatePriceChanges() {
 }
 
 function printBits() {
-	justifyCounter();
-	document.getElementById("bitsCounter").innerHTML = "Bits: " + bits.toFixed(1);
+	bitsPerSec = keyloggerAmount * 0.1 + phishingSiteAmount * 1 + botnetAmount * 10 + superComputerAmount * 100;
+	document.getElementById("bitsCounter").innerHTML = "Bits: " + numberToLetter(bits.toFixed(1)) + "<br/>Bits Per Sec: " + numberToLetter(bitsPerSec.toFixed(1)) + "<br/>Bits Per Click: " + numberToLetter(bitsPerClick.toFixed(1));
 }
 
-function justifyCounter() {
-	var length = bits.toString().length - 6;
-	document.getElementById("bitsCounter").style.left = length + "%";
+function numberToLetter(number) {
+	if (number < 1000)
+		return number;
+
+	for (let [i, v] of largeNums.entries()) {
+		if ((number / v) < 1000)
+			return (number / v).toFixed(2) + largeNumAbbr[i];
+	}
 }
 
 function printKeyloggers() {
-	document.getElementById("keyloggerBtn").innerHTML = "Keyloggers: " + keyloggerAmount + `&nbsp;&nbsp;&nbsp;&nbsp;` + "Bits/Sec: " + (keyloggerAmount * 0.1).toFixed(1) + `<br>` + "Price: " + keyloggerPrice;
+	document.getElementById("keyloggerBtn").innerHTML = "Keyloggers: " + keyloggerAmount + `&nbsp;&nbsp;&nbsp;&nbsp;` + "Bits/Sec: " + numberToLetter((keyloggerAmount * 0.1).toFixed(1)) + `<br>` + "Price: " + numberToLetter(keyloggerPrice);
 }
 
 function printPhishingSites() {
-	document.getElementById("phishingSiteBtn").innerHTML = "Phishing Sites: " + phishingSiteAmount + `&nbsp;&nbsp;&nbsp;&nbsp;` + "Bits/Sec: " + (phishingSiteAmount * 1).toFixed(1) + `<br>` + "Price: " + phishingSitePrice;
+	document.getElementById("phishingSiteBtn").innerHTML = "Phishing Sites: " + phishingSiteAmount + `&nbsp;&nbsp;&nbsp;&nbsp;` + "Bits/Sec: " + numberToLetter((phishingSiteAmount * 1).toFixed(1)) + `<br>` + "Price: " + numberToLetter(phishingSitePrice);
 }
 
 function printBotnets() {
-	document.getElementById("botnetBtn").innerHTML = "Botnets: " + botnetAmount + `&nbsp;&nbsp;&nbsp;&nbsp;` + "Bits/Sec: " + (botnetAmount * 10).toFixed(1) + `<br>` + "Price: " + botnetPrice;
+	document.getElementById("botnetBtn").innerHTML = "Botnets: " + botnetAmount + `&nbsp;&nbsp;&nbsp;&nbsp;` + "Bits/Sec: " + numberToLetter((botnetAmount * 10).toFixed(1)) + `<br>` + "Price: " + numberToLetter(botnetPrice);
 }
 
 function printSuperComputers() {
-	document.getElementById("superComputerBtn").innerHTML = "Super Computers: " + superComputerAmount + `&nbsp;&nbsp;&nbsp;&nbsp;` + "Bits/Sec: " + (superComputerAmount * 100).toFixed(1) + `<br>` + "Price: " + superComputerPrice;
+	document.getElementById("superComputerBtn").innerHTML = "Super Computers: " + superComputerAmount + `&nbsp;&nbsp;&nbsp;&nbsp;` + "Bits/Sec: " + numberToLetter((superComputerAmount * 100).toFixed(1)) + `<br>` + "Price: " + numberToLetter(superComputerPrice);
+}
+
+var obj = {
+	clickerAchievementTracker: function() {
+		for (let [i, value] of clickerAchievements.entries()) {
+			if (buttonClicked >= value) {
+				clickerUpgradesUnlocked[i] = true;
+				clickerUpgrades[i].style.display = "inline-block";
+			}
+		}
+	}
 }
 
 window.setInterval(function() {
